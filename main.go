589f58eef91346a3d954bd21e61cc15526ecd08a
balatsanandrey25 <balatsanandrey25@gmail.com>
2025-11-03 00:54:20 +0600

    MultiTable

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+
+	fmt.Println(MultiTable(5))
+}
+func MultiTable(number int) string {
+	//good luck
+	result := ""
+	for i := 1; i <= 10; i++ {
+		if i < 10 {
+			result += fmt.Sprintf("%d * %d = %d\n", i, number, i*number)
+		} else {
+			result += fmt.Sprintf("%d * %d = %d", i, number, i*number)
+		}
+	}
+	return result
 
 }
