54861404c6d9b8c67a98d0602887aaf816744654
balatsanandrey25 <balatsanandrey25@gmail.com>
2025-11-08 21:30:34 +0600

    chan

--- main.go
+++ main.go
@@ -2,52 +2,36 @@ package main
 
 import (
 	"fmt"
-	"sort"
+	"time"
 )
 
+func writer() <-chan int {
+	in := make(chan int)
+	go func() {
+		for i := range 10 {
+			in <- i + 1
+		}
+		close(in)
+	}()
+	return in
+}
+func double(ch <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for v := range ch {
+			out <- v * 2
+			time.Sleep(1000 * time.Millisecond)
+		}
+		close(out)
+	}()
+
+	return out
+}
+func reader(ch <-chan int) {
+	for v := range ch {
+		fmt.Println(v)
+	}
+}
 func main() {
-	ln1 := []int{1, 2, 3, 4}
-	ln2 := []int{1}
-	fmt.Println(findMedianSortedArrays(ln1, ln2))
-}
-
-func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	if len(nums1) == 0 && len(nums2) == 0 {
-		return float64(0)
-	}
-	if len(nums2) == 0 {
-
-		return getMidle(nums1)
-	}
-	if len(nums1) == 0 {
-
-		return getMidle(nums2)
-	}
-
-	p1, n1 := nums1[0], nums1[len(nums1)-1]
-	p2, n2 := nums2[0], nums2[len(nums2)-1]
-
-	if n1 <= p2 {
-		return getMidle(append(nums1, nums2...))
-	}
-	if n2 <= p1 {
-		return getMidle(append(nums2, nums1...))
-	}
-
-	sliceRes := append(nums1, nums2...)
-	sort.Ints(sliceRes)
-	return getMidle(sliceRes)
-}
-
-// Функция выдает середину слайса
-func getMidle(slice []int) float64 {
-	lenS := len(slice)
-	if lenS == 1 {
-		return float64(slice[0])
-	}
-	mid := lenS / 2
-	if lenS%2 == 0 {
-		return float64(slice[mid-1]+slice[mid]) / 2.0
-	}
-	return float64(slice[mid])
+	reader(double(writer()))
 }
