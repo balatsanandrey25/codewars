e28773fa65a7f1190820ec5d09a99b7823988672
balatsanandrey25 <AyBalatsan>
2025-11-15 00:05:13 +0600

    task_wg+mu

--- main.go
+++ main.go
@@ -1,30 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
 func main() {
-	nums1 := []int{1, 2, 2, 0, 0, 0}
-	m := 3
-	nums2 := []int{2, 2, 6}
-	n := 3
-	merge(nums1, m, nums2, n)
-}
-func merge(nums1 []int, m int, nums2 []int, n int) {
-	f1 := m - 1
-	f2 := n - 1
-	full := m + n - 1
-	for f1 >= 0 && f2 >= 0 {
-		if nums1[f1] >= nums2[f2] {
-			nums1[full] = nums1[f1]
-			f1--
-		} else {
-			nums1[full] = nums2[f2]
-			f2--
-		}
-		full--
+	wg := sync.WaitGroup{}
+	// mu := sync.Mutex{}
+	var counter int64 = 0
+	// or
+	// counter := 0
+	for i := 1; i <= 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			// defer mu.Unlock()
+			// mu.Lock()
+			atomic.AddInt64(&counter, 1)
+			// counter++
+		}()
 	}
-	for f2 >= 0 {
-		nums1[full] = nums2[f2]
-		f2--
-		full--
-	}
+	wg.Wait()
+	fmt.Println(counter)
 
 }
