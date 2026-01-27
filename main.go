d2facc9e78ac6cb20770effec555dc6b91d759b7
balatsanandrey25 <AyBalatsan>
2026-01-27 18:52:14 +0600

    IsUpperCase

--- main.go
+++ main.go
@@ -1,28 +1,16 @@
 package main
 
-import "fmt"
+import "strings"
 
-func DigPow(n, p int) int {
-	// your code (ap+bp+1+cp+2+dp+3+...)=n∗k
-	str := fmt.Sprintf("%d", n)
-	result := 0.0
-	for i, char := range str {
-		result += float64(powInt(int(char-'0'), p+i))
-	}
-	k := result / float64(n)
-	if k == float64(int(k)) {
-		return int(k)
-	} else {
-		return -1
-	}
-}
-func powInt(base, exponent int) int {
-	result := 1
-	for i := 0; i < exponent; i++ {
-		result *= base
-	}
-	return result
+type MyString string
+
+func (s MyString) IsUpperCase() bool {
+	// Your code here!
+	str := string(s)
+
+	return str == strings.ToUpper(str)
+
 }
 func main() {
-	fmt.Println(DigPow(46288, 3))
+
 }
